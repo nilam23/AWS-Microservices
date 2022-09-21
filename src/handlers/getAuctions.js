8b5f65c4543abce5d6afcdc7049e15eb64ba17c7
nilam23 <nilampathak72@gmail.com>
2022-09-21 23:52:57 +0530

    scan changed to query and schema validation added

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,19 +1,32 @@
 import AWS from 'aws-sdk';
-import commonMiddleware from '../lib/commonMiddleware';
 import createError from 'http-errors';
+import validator from '@middy/validator';
+import commonMiddleware from '../lib/commonMiddleware';
+import getAuctionsSchema from '../lib/schemas/getAuctionsSchema';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function getAuctions(event, context) {
+  const { status } = event.queryStringParameters;
   let auctions;
 
-  // performing a scan operation to fetch the auctions from db
+  // performing a query operation to fetch the auctions from db according to the status
   try {
-    const results = await dynamodb.scan({
+    const params = {
       TableName: process.env.AUCTIONS_TABLE_NAME,
-    }).promise();
+      IndexName: 'statusAndEndDate',
+      KeyConditionExpression: '#status = :status', // since 'status' is a reserved word
+      ExpressionAttributeValues: {
+        ':status': status
+      },
+      ExpressionAttributeNames: {
+        '#status': 'status'
+      }
+    };
 
-    auctions = results.Items;
+    // querying the db with the partition key (GSI)
+    const result = await dynamodb.query(params).promise();
+    auctions = result.Items;
   } catch (error) {
     console.log('Error in fetching the auctions', error);
     throw new createError.InternalServerError(error);
@@ -26,4 +39,12 @@ async function getAuctions(event, context) {
   };
 }
 
-export const handler = commonMiddleware(getAuctions);
+export const handler = commonMiddleware(getAuctions).use(
+  validator({
+    inputSchema: getAuctionsSchema,
+    ajvOptions: {
+      useDefaults: true,
+      strict: false,
+    },
+  })
+);
