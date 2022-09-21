7e87516fc1194f36d40fda8bc3fea1c1a74d3887
nilam23 <nilampathak72@gmail.com>
2022-09-21 23:52:26 +0530

    schema validation added

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -1,7 +1,9 @@
 import AWS from 'aws-sdk';
-import commonMiddleware from '../lib/commonMiddleware';
 import createError from 'http-errors';
+import validator from '@middy/validator';
 import { getAuctionById } from './getAuction';
+import commonMiddleware from '../lib/commonMiddleware';
+import placeBidSchema from '../lib/schemas/placebidSchema';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -51,4 +53,11 @@ async function placeBid(event, context) {
   };
 }
 
-export const handler = commonMiddleware(placeBid);
+export const handler = commonMiddleware(placeBid).use(
+  validator({
+    inputSchema: placeBidSchema,
+    ajvOptions: {
+      strict: false,
+    },
+  })
+);
---
+++ src/lib/schemas/placebidSchema.js
@@ -0,0 +1,18 @@
+const schema = {
+  properties: {
+    body: {
+      type: 'object',
+      properties: {
+        title: {
+          type: 'number'
+        }
+      },
+      required: ['amount']
+    }
+  },
+  required: [
+    'body'
+  ]
+};
+
+export default schema;
